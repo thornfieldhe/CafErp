412fac529a1112f425bd2441f7b8fa03dc09ff07
何翔华 <thornfield_he@outlook.com>
2015-08-21 17:59:56 +0800

    初始化

---
+++ Erp.Cms/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Erp.Cms.Startup))]
+namespace Erp.Cms
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
