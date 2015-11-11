d7d0ec4d463a3715e9518c9e67902a1a40e62011
何翔华 <thornfield_he@outlook.com>
2015-11-12 00:19:55 +0800

    增加打印功能

---
+++ Erp.Printer/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Erp.Printer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.导入ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.导入ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(3, 3);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1058, 29);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 导入ToolStripMenuItem
+            // 
+            this.导入ToolStripMenuItem.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.导入ToolStripMenuItem.Name = "导入ToolStripMenuItem";
+            this.导入ToolStripMenuItem.Size = new System.Drawing.Size(54, 25);
+            this.导入ToolStripMenuItem.Text = "导入";
+            this.导入ToolStripMenuItem.Click += new System.EventHandler(this.导入ToolStripMenuItem_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox1.Location = new System.Drawing.Point(3, 32);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(10);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.groupBox1.Size = new System.Drawing.Size(1058, 100);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "搜索文档";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(23, 44);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(974, 29);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.listView1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 132);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1058, 370);
+            this.panel1.TabIndex = 2;
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1058, 370);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(128, 128);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1064, 505);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(3);
+            this.Text = "表单打印工具";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 导入ToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+    }
+}
+
---
+++ Erp.Printer/Form1.cs
@@ -0,0 +1,158 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Form1.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The form 1.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Erp.Printer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing.Printing;
+    using System.IO;
+    using System.Linq;
+    using System.Windows.Forms;
+
+    using TAF.Utility;
+
+    /// <summary>
+    /// The form 1.
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// The path.
+        /// </summary>
+        private readonly string path = @"documents\";
+
+        private List<ListViewItem> Items = new List<ListViewItem>();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Form1"/> class.
+        /// </summary>
+        public Form1()
+        {
+
+            InitializeComponent();
+            imageList1.Images.Add(System.Drawing.Image.FromFile(@"Icons\excel.png"));
+            imageList1.Images.Add(System.Drawing.Image.FromFile(@"Icons\word.png"));
+            imageList1.Images.Add(System.Drawing.Image.FromFile(@"Icons\pdf.png"));
+            imageList1.Images.Add(System.Drawing.Image.FromFile(@"Icons\image.png"));
+            imageList1.Images.Add(System.Drawing.Image.FromFile(@"Icons\empty.png"));
+            LoadData();
+            listView1.Items.AddRange(Items.OrderBy(i => i.Text).ToArray());
+            listView1.SelectedIndexChanged += ListView1_SelectedIndexChanged;
+            
+        }
+
+        private void ListView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 1)
+            {
+                var filePath = listView1.SelectedItems[0].Tag.ToString();
+                Print(filePath);
+            }
+
+        }
+
+        private void ListView1_ItemChecked(object sender, ItemCheckedEventArgs e)
+        {
+            var filePath = e.Item.Tag.ToString();
+            Print(filePath);
+
+        }
+
+
+
+
+        /// <summary>
+        /// The text box 1_ text changed.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            listView1.SelectedIndexChanged -= ListView1_SelectedIndexChanged;
+            var newItems = Items.Where(i => i.Text.Contains(textBox1.Text.Trim()) || i.Text.GetChineseSpell().Contains(textBox1.Text.Trim().GetChineseSpell())).ToList();
+            listView1.Items.Clear();
+            listView1.Items.AddRange(newItems.OrderBy(i => i.Text).ToArray());
+
+            listView1.SelectedIndexChanged += ListView1_SelectedIndexChanged;
+
+        }
+
+
+        /// <summary>
+        /// The load data.
+        /// </summary>
+        public void LoadData()
+        {
+
+            Items.Clear();
+            var dir = new DirectoryInfo(path);
+            foreach (var d in dir.GetFiles())
+            {
+                var ext = Path.GetExtension(path + d.Name);
+                var item = new ListViewItem { Text = d.Name, Tag = this.path + d.Name };
+                if (ext == ".jpg" || ext == ".jpeg" || ext == ".gif")
+                {
+                    item.ImageIndex = 3;
+                }
+                else if (ext == ".pdf")
+                {
+                    item.ImageIndex = 2;
+                }
+                else if (ext == ".doc" || ext == ".docx")
+                {
+                    item.ImageIndex = 1;
+                }
+                else if (ext == ".xls" || ext == ".xlsx")
+                {
+                    item.ImageIndex = 0;
+                }
+                else
+                {
+                    item.ImageIndex = 4;
+                }
+
+                if (ext.In(new List<string> { ".jpg", ".jpeg", ".gif", ".pdg", ".doc", ".docx", ".xls", ".xlsx", ".txt", ".pdf" }))
+                {
+                    Items.Add(item);
+                }
+            }
+        }
+
+        private void 导入ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "选择文件";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                listView1.SelectedIndexChanged -= ListView1_SelectedIndexChanged;
+                var names = openFileDialog1.FileNames;
+                names.ForEach(
+                              n =>
+                              {
+                                  File.Copy(n, Directory.GetCurrentDirectory() + @"\" + path + Path.GetFileName(n), true);
+                              });
+                LoadData();
+                listView1.Items.Clear();
+                listView1.Items.AddRange(Items.OrderBy(i=>i.Text).ToArray());
+                listView1.SelectedIndexChanged += ListView1_SelectedIndexChanged;
+            }
+        }
+
+        private void Print(string filePath)
+        {
+            printDocument1.PrinterSettings = new PrinterSettings() { PrintFileName = filePath, PaperSizes = { new PaperSize() { PaperName = "A4" } } };
+            printDocument1.Print();
+        }
+
+    }
+}
